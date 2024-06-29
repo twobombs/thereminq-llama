711a5ed05ad267c2264289d75de2959b07b94f90
Aryan Blaauw <ablaauw@gmx.net>
2024-06-29 21:49:40 +0200

    Create run-hydrogen.py

---
+++ runfiles/run-hydrogen.py
@@ -0,0 +1,31 @@
+from qiskit import Aer, execute
+from qiskit.circuit.library import TwoLocal
+from qiskit.algorithms import VQE
+from qiskit.algorithms.optimizers import SLSQP
+from qiskit.quantum_info import Pauli
+from qiskit.opflow import I, Z, X, Y, StateFn
+from qiskit_nature.problems.second_quantization.electronic import ElectronicStructureProblem
+from qiskit_nature.converters.second_quantization import QubitConverter
+from qiskit_nature.mappers.second_quantization import JordanWignerMapper
+from qiskit_nature.drivers.second_quantization import PySCFDriver
+from qiskit_nature.units import DistanceUnit
+
+# Define molecular parameters
+molecule = "H .0 .0 .0; H .0 .0 0.735"
+driver = PySCFDriver(molecule, basis="sto3g", unit=DistanceUnit.ANGSTROM)
+problem = ElectronicStructureProblem(driver)
+
+# Convert the problem to a qubit Hamiltonian
+qubit_converter = QubitConverter(mapper=JordanWignerMapper())
+qubit_hamiltonian = qubit_converter.convert(problem.second_q_ops()[0])
+
+# Set up the ansatz and optimizer
+ansatz = TwoLocal(rotation_blocks='ry', entanglement_blocks='cz')
+optimizer = SLSQP(maxiter=100)
+
+# Set up the VQE algorithm
+vqe = VQE(ansatz, optimizer, quantum_instance=Aer.get_backend('statevector_simulator'))
+vqe_result = vqe.compute_minimum_eigenvalue(qubit_hamiltonian)
+
+# Output the results
+print(f"Computed ground state energy: {vqe_result.eigenvalue.real} Hartree")
