2900597b46ca5ace94db2fb13eab3c8c25dac626
Aryan Blaauw <ablaauw@gmx.net>
2024-08-29 10:15:12 +0000

    Add files via upload

---
+++ misc/pull.sh
@@ -0,0 +1,23 @@
+
+for run in {0..9}; do wget https://huggingface.co/NousResearch/Hermes-3-Llama-3.1-405B/resolve/main/model-0000$run-of-00191.safetensors; done &
+
+for run in {10..19}; do wget https://huggingface.co/NousResearch/Hermes-3-Llama-3.1-405B/resolve/main/model-000$run-of-00191.safetensors; done &
+for run in {20..29}; do wget https://huggingface.co/NousResearch/Hermes-3-Llama-3.1-405B/resolve/main/model-000$run-of-00191.safetensors; done &
+for run in {30..39}; do wget https://huggingface.co/NousResearch/Hermes-3-Llama-3.1-405B/resolve/main/model-000$run-of-00191.safetensors; done &
+for run in {40..49}; do wget https://huggingface.co/NousResearch/Hermes-3-Llama-3.1-405B/resolve/main/model-000$run-of-00191.safetensors; done &
+for run in {50..59}; do wget https://huggingface.co/NousResearch/Hermes-3-Llama-3.1-405B/resolve/main/model-000$run-of-00191.safetensors; done &
+for run in {60..69}; do wget https://huggingface.co/NousResearch/Hermes-3-Llama-3.1-405B/resolve/main/model-000$run-of-00191.safetensors; done &
+for run in {70..79}; do wget https://huggingface.co/NousResearch/Hermes-3-Llama-3.1-405B/resolve/main/model-000$run-of-00191.safetensors; done &
+for run in {80..89}; do wget https://huggingface.co/NousResearch/Hermes-3-Llama-3.1-405B/resolve/main/model-000$run-of-00191.safetensors; done &
+for run in {90..99}; do wget https://huggingface.co/NousResearch/Hermes-3-Llama-3.1-405B/resolve/main/model-000$run-of-00191.safetensors; done &
+
+for run in {100..109}; do wget https://huggingface.co/NousResearch/Hermes-3-Llama-3.1-405B/resolve/main/model-00$run-of-00191.safetensors; done &
+for run in {110..119}; do wget https://huggingface.co/NousResearch/Hermes-3-Llama-3.1-405B/resolve/main/model-00$run-of-00191.safetensors; done &
+for run in {120..129}; do wget https://huggingface.co/NousResearch/Hermes-3-Llama-3.1-405B/resolve/main/model-00$run-of-00191.safetensors; done &
+for run in {130..139}; do wget https://huggingface.co/NousResearch/Hermes-3-Llama-3.1-405B/resolve/main/model-00$run-of-00191.safetensors; done &
+for run in {140..149}; do wget https://huggingface.co/NousResearch/Hermes-3-Llama-3.1-405B/resolve/main/model-00$run-of-00191.safetensors; done &
+for run in {150..159}; do wget https://huggingface.co/NousResearch/Hermes-3-Llama-3.1-405B/resolve/main/model-00$run-of-00191.safetensors; done &
+for run in {160..169}; do wget https://huggingface.co/NousResearch/Hermes-3-Llama-3.1-405B/resolve/main/model-00$run-of-00191.safetensors; done &
+for run in {170..179}; do wget https://huggingface.co/NousResearch/Hermes-3-Llama-3.1-405B/resolve/main/model-00$run-of-00191.safetensors; done &
+for run in {180..189}; do wget https://huggingface.co/NousResearch/Hermes-3-Llama-3.1-405B/resolve/main/model-00$run-of-00191.safetensors; done &
+for run in {190..191}; do wget https://huggingface.co/NousResearch/Hermes-3-Llama-3.1-405B/resolve/main/model-00$run-of-00191.safetensors; done &
